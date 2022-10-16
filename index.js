b4d1eea8317a49b19e74c198dcbdf5e492308d13
batoul sudki tenbakjy <batoul.tenbakji@gmail.com>
2022-10-16 14:43:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+let num1=31,num2=2;
+const multiply=num1*num2;
+const random=Math.floor(Math.random())+1;
+let num3=24,num4=5;
+const mod=num3%num4;
+const max=Math.max(1,5,20,7,0,-30,18);
